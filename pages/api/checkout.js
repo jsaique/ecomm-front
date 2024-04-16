3104f1ac9b041a520c5c9d142abbe420df88a454
jsaique <jsaique11@gmail.com>
2024-04-16 19:08:35 -0400

    Added coupons

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -63,6 +63,7 @@ export default async function handler(req, res) {
     success_url: process.env.NEXT_PUBLIC_REDIRECT_URL + "/cart?success=1",
     cancel_url: process.env.NEXT_PUBLIC_REDIRECT_URL + "/cart?canceled=1",
     metadata: { orderId: orderData._id.toString() },
+    allow_promotion_codes: true,
     shipping_options: [
       {
         shipping_rate_data: {
--- pages/index.jsx
+++ pages/index.jsx
@@ -26,11 +26,11 @@ export default function HomePage({
 }
 
 export async function getServerSideProps(ctx) {
+  await mongooseConnect();
   const featuredProductSetting = await Setting.findOne({
     name: "featuredProductId",
   });
   const featuredProductId = featuredProductSetting.value;
-  await mongooseConnect();
   const featuredProduct = await Product.findById(featuredProductId);
   const newProducts = await Product.find({}, null, {
     sort: { _id: -1 },
